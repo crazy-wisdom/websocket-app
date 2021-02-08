0ed58610ee651b7f4620839dc5880b3ba2780260
arlyxiao <kingla_pei@163.com>
2021-02-08 17:57:30 +0800

    Change var to const

--- src/services/page-service.tsx
+++ src/services/page-service.tsx
@@ -11,7 +11,7 @@ export const wsUrl = function() {
 }();
 
 export const timeAgo = function(timeCreated: number) {
-  var periods = {
+  const periods = {
     month: 30 * 24 * 60 * 60 * 1000,
     week: 7 * 24 * 60 * 60 * 1000,
     day: 24 * 60 * 60 * 1000,

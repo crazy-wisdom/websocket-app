81880caf3cbf1767946a7477e29853b0de337bd1
arlyxiao <kingla_pei@163.com>
2021-02-08 17:46:13 +0800

    Add error validation

--- src/views/talks/new.tsx
+++ src/views/talks/new.tsx
@@ -13,6 +13,7 @@ import BaseLayout from '../layouts/base-layout';
 const New = () => {
   let [title, setTitle] = React.useState('');
   let [content, setContent] = React.useState('');
+  let [error, setError] = React.useState('');
 
   const history = useHistory();
   const urlPrefix = PageService.urlPrefix;
@@ -21,28 +22,36 @@ const New = () => {
 
   function submitNew(event: React.MouseEvent) {
     let params: any = {};
-    // console.log('Start submit');
-    if (title !== '' && content !== '') {
-      params.title = title;
-      params.content = content;
-      params.rank = 0;
-      params.created_at = new Date().getTime();
 
-      // console.log('Posting data....');
-      // console.log(params);
+    if (title === '') {
+      setError('Title is required.');
+      return;
+    }
 
-      try {
-        axios.post(`${apiUrl}/api/talks`, params)
-        .then(function (response) {
-          // console.log(response);
-          history.push(`${urlPrefix}/talks`);
-        })
-        .catch(function (error) {
-          // console.log(error);
-        });
-      } catch (error) {
-        // console.log(error.message);
-      }
+    if (content === '') {
+      setError('Content is required.');
+      return;
+    }
+
+    params.title = title;
+    params.content = content;
+    params.rank = 0;
+    params.created_at = new Date().getTime();
+
+    // console.log('Posting data....');
+    // console.log(params);
+
+    try {
+      axios.post(`${apiUrl}/api/talks`, params)
+      .then(function (response) {
+        // console.log(response);
+        history.push(`${urlPrefix}/talks`);
+      })
+      .catch(function (error) {
+        // console.log(error);
+      });
+    } catch (error) {
+      // console.log(error.message);
     }
   }
 
@@ -53,17 +62,24 @@ const New = () => {
           <div className="container">
 
             <div className="form new-form">
+
+              {error &&
+                <div className="error">
+                  { error }
+                </div>
+              }
+
               <div className="input-wrap">
 
                 <div className="after-input hide">
                   <div className="input-hint">Title</div>
-                  <input className="title-input" type="text" name="title" id="talk-title" onChange={e => setTitle(e.target.value)} defaultValue={ `${title}` } />
+                  <input className="title-input" type="text" name="title" id="talk-title" onChange={e => setTitle(e.target.value.trim())} defaultValue={ `${title}` } />
                 </div>
 
               </div>
 
               <div className="text-wrap">
-                <textarea placeholder="Share something...." id="talk-content" onChange={e => setContent(e.target.value)} defaultValue={ `${content}` }></textarea>
+                <textarea placeholder="Share something...." id="talk-content" onChange={e => setContent(e.target.value.trim())} defaultValue={ `${content}` }></textarea>
               </div>
 
               <button className="submit-btn" onClick={submitNew}>Submit</button>
